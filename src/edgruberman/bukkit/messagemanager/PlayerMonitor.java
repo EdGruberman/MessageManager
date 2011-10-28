4d2f657e175f6de6c7152507adf68cec7c4c6902
EdGruberman <ed@rjump.com>
2011-10-28 08:10:37 -0700

    FIX: World messages sometimes not showing after player moves between worlds.

--- src/edgruberman/bukkit/messagemanager/PlayerMonitor.java
+++ src/edgruberman/bukkit/messagemanager/PlayerMonitor.java
@@ -1,15 +1,11 @@
 package edgruberman.bukkit.messagemanager;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.Plugin;
 
 import edgruberman.bukkit.messagemanager.channels.Channel;
@@ -18,12 +14,9 @@ import edgruberman.bukkit.messagemanager.channels.ServerChannel;
 import edgruberman.bukkit.messagemanager.channels.WorldChannel;
 
 /**
- * Monitor player joins, quits, and world changes to ensure Player, Server,
- * and World channels are properly configured and ready to send messages.
+ * Ensure Player, Server, and World channels have members properly updated.
  */
 final class PlayerMonitor extends PlayerListener {
-    
-    Map<Player, Location> last = new HashMap<Player, Location>();
     
     PlayerMonitor(final Plugin plugin) {
         // Create player channels for all existing players
@@ -31,40 +24,28 @@ final class PlayerMonitor extends PlayerListener {
             PlayerChannel.getInstance(player);
         
         plugin.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_JOIN, this, Event.Priority.Monitor, plugin);
-        plugin.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_TELEPORT, this, Event.Priority.Monitor, plugin);
+        plugin.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_CHANGED_WORLD, this, Event.Priority.Monitor, plugin);
         plugin.getServer().getPluginManager().registerEvent(Event.Type.PLAYER_QUIT, this, Event.Priority.Monitor, plugin);
     }
     
     @Override
     public void onPlayerJoin(final PlayerJoinEvent event) {
-        this.last.put(event.getPlayer(), event.getPlayer().getLocation());
         PlayerChannel.getInstance(event.getPlayer());
         ServerChannel.getInstance(event.getPlayer().getServer()).addMember(event.getPlayer());
         WorldChannel.getInstance(event.getPlayer().getWorld()).addMember(event.getPlayer());
     }
     
     @Override
-    public void onPlayerTeleport(final PlayerTeleportEvent event) {
-        if (event.isCancelled()) return;
+    public void onPlayerChangedWorld(final PlayerChangedWorldEvent event) {
+        Main.messageManager.log("Player " + event.getPlayer().getName() + " changed world from [" + event.getFrom().getName() + "] teleporting to [" + event.getPlayer().getWorld().getName() + "]", MessageLevel.FINEST);
         
-        Location last = this.last.get(event.getPlayer());
-        Main.messageManager.log("Player " + event.getPlayer().getName() + " last recorded at [" + (last != null ? last.getWorld().getName() : "") + "] teleporting to [" + event.getTo().getWorld().getName() + "]", MessageLevel.FINEST);
-        if (last == null) {
-            this.last.put(event.getPlayer(), event.getTo());
-            return;
-        }
-        
-        if (last.getWorld().equals(event.getTo().getWorld())) return;
-        
-        this.last.put(event.getPlayer(), event.getTo());
-        WorldChannel.getInstance(last.getWorld()).removeMember(event.getPlayer());
-        WorldChannel.getInstance(event.getTo().getWorld()).addMember(event.getPlayer());
+        WorldChannel.getInstance(event.getFrom()).removeMember(event.getPlayer());
+        WorldChannel.getInstance(event.getPlayer().getWorld()).addMember(event.getPlayer());
     }
     
     @Override
     public void onPlayerQuit(final PlayerQuitEvent event) {
         Channel.disconnect(event.getPlayer());
         PlayerChannel.disposeInstance(event.getPlayer());
-        this.last.remove(event.getPlayer());
     }
 }
